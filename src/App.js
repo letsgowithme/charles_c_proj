e0a313ad3d0bff9cf7c2da52c848fc77101dc56d
iryna <musicira@gmail.com>
2023-12-13 19:38:41 +0100

    App.js update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PricesServices from "./components/pages/pricesServices/PricesServices";
 import Contact from "./components/pages/contact/Contact";
 import MentionsLegales from "./components/pages/mentionsLegales/MentionsLegales";
 import PolicyPrivacy from "./components/pages/policyPrivacy/PolicyPrivacy";
+import CacheClear from "./components/admin/CacheClear";
 // import Navbar from "./components/navbar/Navbar";
 
 
@@ -22,8 +23,13 @@ return (
 
 <Route path="/mentionsLegales" element={<MentionsLegales/>}/>
 <Route path="/policyPrivacy" element={<PolicyPrivacy/>}/>
+<Route path="/cacheClear" element={<CacheClear/>}/>
 {/* <Route path="*" element={<NotFound/>}/> */}
 </Routes>
+
 </>
 );
+
 }
+
+
